d30352baa19cbf11a9d3422735e6aeb71a39b5ed
wanshun <1150123599@qq.com>
2019-06-08 20:43:46 +0800

    项目提交

---
+++ tsgit_1/src/com/itheima/domain/A.java
@@ -0,0 +1,13 @@
+package com.itheima.domain;
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println("1111111");
+        System.out.println("1111111");
+        System.out.println("1111111");
+        System.out.println("1111111");
+        System.out.println("1111111");
+        System.out.println("1111111");
+        System.out.println("1111111");
+    }
+}
